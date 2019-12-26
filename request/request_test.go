e925a9e807479d0a8927c052c104124452b434ae
Eore <eore26@gmail.com>
2019-12-26 11:27:39 +0700

    update unit test

--- request/request_test.go
+++ request/request_test.go
@@ -1,14 +1,14 @@
 package request_test
 
 import (
-	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"testing"
 
 	"github.com/Eore/libraery/request"
 )
 
-const url = "https://jsonplaceholder.typicode.com"
+const baseURL = "https://reqres.in/api"
 
 type response struct {
 	UserID int    `json:"userId"`
@@ -17,38 +17,55 @@ type response struct {
 	Body   string `json:"body"`
 }
 
-var testGET map[string]response = map[string]response{
-	"/posts/1": response{
-		UserID: 1,
-		ID:     1,
-		Title:  "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-		Body:   "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
-	},
-	"/posts/2": response{
-		UserID: 1,
-		ID:     2,
-		Title:  "qui est esse",
-		Body:   "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla",
-	},
+type testGET struct {
+	Status   int
+	URL      string
+	Response string
+}
+
+var listTestGET []testGET = []testGET{
+	{200, baseURL + "/users/1", `{"data":{"id":1,"email":"george.bluth@reqres.in","first_name":"George","last_name":"Bluth","avatar":"https://s3.amazonaws.com/uifaces/faces/twitter/calebogden/128.jpg"}}`},
+	{200, baseURL + "/users/2", `{"data":{"id":2,"email":"janet.weaver@reqres.in","first_name":"Janet","last_name":"Weaver","avatar":"https://s3.amazonaws.com/uifaces/faces/twitter/josephstein/128.jpg"}}`},
+	{200, baseURL + "/users/3", `{"data":{"id":3,"email":"emma.wong@reqres.in","first_name":"Emma","last_name":"Wong","avatar":"https://s3.amazonaws.com/uifaces/faces/twitter/olegpogodaev/128.jpg"}}`},
+	{200, baseURL + "/users/4", `{"data":{"id":4,"email":"eve.holt@reqres.in","first_name":"Eve","last_name":"Holt","avatar":"https://s3.amazonaws.com/uifaces/faces/twitter/marcoramires/128.jpg"}}`},
+	{200, baseURL + "/users/5", `{"data":{"id":5,"email":"charles.morris@reqres.in","first_name":"Charles","last_name":"Morris","avatar":"https://s3.amazonaws.com/uifaces/faces/twitter/stephenmoon/128.jpg"}}`},
+	{404, baseURL + "/users/23", `{}`},
+	{200, baseURL + "/unknown/1", `{"data":{"id":1,"name":"cerulean","year":2000,"color":"#98B2D1","pantone_value":"15-4020"}}`},
 }
 
 func TestGET(t *testing.T) {
-	for key, value := range testGET {
-		t.Run(key, func(t *testing.T) {
-			res := request.GET(url+key, nil)
-			var rsp response
-			json.NewDecoder(res.Body).Decode(&rsp)
-			if rsp != value {
-				t.Fatalf("\ndata yang di dapat :\n%+v \nharusnya :\n%+v", rsp, value)
+	for i, value := range listTestGET {
+		t.Run(fmt.Sprint(i), func(t *testing.T) {
+			res := request.GET(value.URL, nil)
+			b, _ := ioutil.ReadAll(res.Body)
+			if res.StatusCode != value.Status || string(b) != value.Response {
+				t.Fatalf("\nexpected :\n\tstatus -> %d\n\tresponse -> %s\ngot :\n\tstatus -> %d\n\tresponse -> %s", value.Status, value.Response, res.StatusCode, string(b))
 			}
 		})
 	}
 }
 
+type testPOST struct {
+	Status int
+	URL    string
+	Body   string
+}
+
+var listTestPOST []testPOST = []testPOST{
+	{201, baseURL + "/users", `{"miaw", "lala"}`},
+	{201, baseURL + "/users", `{"miao", "lili"}`},
+	{201, baseURL + "/users", `{"meow", "lulu"}`},
+	{201, baseURL + "/users", `{"miau", "lele"}`},
+	{201, baseURL + "/users", `{"meou", "lolo"}`},
+}
+
 func TestPOST(t *testing.T) {
-	t.Run("test", func(t *testing.T) {
-		res := request.POST(url+"/posts", `{"title":"foo","body":"bar","userId":1}`, nil)
-		b, _ := ioutil.ReadAll(res.Body)
-		t.Fatal(string(b))
-	})
+	for i, value := range listTestPOST {
+		t.Run(fmt.Sprint(i), func(t *testing.T) {
+			res := request.POST(value.URL, value.Body, nil)
+			if res.StatusCode != value.Status {
+				t.Fatalf("\nexpected :\n\tstatus -> %d\ngot :\n\tstatus -> %d", value.Status, res.StatusCode)
+			}
+		})
+	}
 }
